e08cac0a081dc1d75f7824dbe286ae95d249ba45
Ahmet Onat <onat@sabanciuniv.edu>
2016-04-21 23:30:17 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,24 @@
+/********************************************************************
+ * Copyright 2016 Ahmet Onat
+ * This program is distributed under the terms of the 
+ * GNU General Public License
+ *
+ * This file is part of STM32F10X-GCC
+ *
+ * STM32F10X-GCC is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * STM32F10X-GCC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with AH_HSI.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *********************************************************************/
+ 
 # include <stm32f10x.h>
 # include <stm32f10x_rcc.h>
 # include <stm32f10x_gpio.h>
